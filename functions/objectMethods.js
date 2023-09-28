0c2709dbbb5b32cf15b24cc65b0fd1b509f96fee
Combatd <combatdstar@gmail.com>
2023-09-28 13:20:55 -0700

    Access specific instance within student object using this keyword

--- functions/objectMethods.js
+++ functions/objectMethods.js
@@ -2,8 +2,8 @@ var student = {
   id: 1,
   name: 'Bob',
   display: function() {
-    console.log(student.id);
-    console.log(student.name);
+    console.log(this.id); // this carries the object that you are eaccessing
+    console.log(this.name);
   }
 };
 
