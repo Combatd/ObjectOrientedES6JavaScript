1e5c25167d818ea514c54267155c2711b172f2e9
Combatd <combatdstar@gmail.com>
2023-09-28 17:44:17 -0700

    Invoke #displayDetails.call()

--- functions/objectMethods.js
+++ functions/objectMethods.js
@@ -1,6 +1,8 @@
-function displayDetails() {
+function displayDetails(testscore, testscore2) {
   console.log(this.id);
   console.log(this.name);
+  console.log(testscore);
+  console.log(testscore2);
 }
 
 var student1 = {
@@ -18,12 +20,17 @@ id = 3;
 name = "Mary";
 
 // call will invoke with a parameter becoming the context, or 'this'
-displayDetails.call(student1);
+// displayDetails.call(student1, 10);
 
-displayDetails.call(student2);
+// displayDetails.call(student2, 20);
 
-displayDetails.call();
+// displayDetails.call(global, 30);
 
 // student1.display();
 // student2.display();
 // displayDetails();
+
+// apply() is similar to call but takes an entire array as a parameter
+displayDetails.apply(student1, [10, 20, 30]);
+displayDetails.apply(student2, [20, 30]);
+displayDetails.apply(global, [30, 15]);
