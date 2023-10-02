7d5712246fe3414fa19c4f13688075c2348f9e79
Combatd <combatdstar@gmail.com>
2023-10-01 17:40:42 -0700

    Indrectly invoke put and set methods of objects"

---
+++ objects/propertiesInternals.js
@@ -0,0 +1,10 @@
+let course1 = {
+  name: "JavaScript fundamentals" // [[Put]]
+}
+
+let course2 = new Object();
+course2.name = 'ReactJS'; //[[Put]]
+
+course1.name = 'End to End Java Project'; // [[Set]] Setting a value on an existing object
+
+course1.description = 'Master Java Project Development'; // [[Put]] New property added to the object
