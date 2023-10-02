2567e4753acbda64f166166485ab28d70e45640d
Combatd <combatdstar@gmail.com>
2023-10-01 18:08:48 -0700

    Use in operator"

--- objects/propertiesInternals.js
+++ objects/propertiesInternals.js
@@ -8,3 +8,6 @@ course2.name = 'ReactJS'; //[[Put]]
 course1.name = 'End to End Java Project'; // [[Set]] Setting a value on an existing object
 
 course1.description = 'Master Java Project Development'; // [[Put]] New property added to the object
+
+console.log('name' in course1); // true
+console.log('description' in course2); // false
