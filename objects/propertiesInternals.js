711505da0c7fb81b10055281a0904692c49c4a09
Combatd <combatdstar@gmail.com>
2023-10-01 18:34:04 -0700

    Delete corus1.description attribute

--- objects/propertiesInternals.js
+++ objects/propertiesInternals.js
@@ -14,3 +14,6 @@ console.log('description' in course2); // false
 
 console.log('toString' in course1); // true, Object instances always have toString method
 console.log(course1.hasOwnProperty('toString')); // false, for specific attribute
+
+delete course1.description; // return true if attribute is deleted
+console.log('description' in course1, ' description is not on course1'); // fa;se
