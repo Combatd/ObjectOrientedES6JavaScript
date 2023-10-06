007c813b6b8e4fbf9eb93152bc820896513eeeec
Combatd <combatdstar@gmail.com>
2023-10-06 04:07:09 -0700

    Set configurable attribute

--- objects/propertyAttributes.js
+++ objects/propertyAttributes.js
@@ -9,3 +9,17 @@ Object.defineProperty(creditCard, 'name', {
 
 console.log('name' in creditCard); // => true
 console.log(creditCard.propertyIsEnumerable('name')); // => false, Object.keys or Object.values won't show this attribute
+
+// configurable determines if attribute can be modified in the future
+Object.defineProperty(creditCard, 'name', {
+  configurable: false
+});
+
+delete creditCard.name; // => doesn't work, doesn't throw error unless in strict mode
+console.log('name' in creditCard); // => true
+console.log(creditCard.name); // -> undefined
+
+// cannot redefine configurable after setting it to false
+Object.defineProperty(creditCard, 'name', {
+  configurable: true
+});
