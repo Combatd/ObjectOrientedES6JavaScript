584f07af823ab12b00530ec6a723f0e1c0e8f10f
Combatd <combatdstar@gmail.com>
2023-10-06 02:16:03 -0700

    Modify and set hidden encapsulated data

---
+++ objects/accessorProperties.js
@@ -0,0 +1,20 @@
+// accessor and mutator methods
+
+// getter and setter methods
+let creditCard = {
+  _name: 'John',
+
+  get name() {
+    return this._name;
+  },
+
+  set name(value) {
+    this._name = value;
+  }
+}
+
+console.log(creditCard.name);
+creditCard.name = 'Bob';
+console.log(creditCard.name);
+
+// getters and setters only need to be defined if there are special things happening liek calculations/logic
