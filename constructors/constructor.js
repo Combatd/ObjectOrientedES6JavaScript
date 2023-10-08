5715cc6f0bac49714c5d0c43829b23118b3b4e62
Combatd <combatdstar@gmail.com>
2023-10-08 15:33:16 -0700

    Define Flight#display

--- constructors/constructor.js
+++ constructors/constructor.js
@@ -1,9 +1,18 @@
-function Flight() {
+function Flight(airlines, flightNumber) {
+  this.airlines = airlines;
+  this.flightNumber = flightNumber;
 
+  this.display = function() {
+    console.log(this.airlines);
+    console.log(this.flightNumber);
+  };
 }
 
-let flight1 = new Flight();
-let flight2 = new Flight(); 
+let flight1 = new Flight('American Airlines', 'AA123');
+let flight2 = new Flight('South West', 'SW456'); 
+
+flight1.display();
+flight2.display();
 
 console.log(flight1 instanceof Flight);
 console.log(flight2 instanceof Flight);
