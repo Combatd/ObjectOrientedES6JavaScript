13a62c9a150ff748bf3baf6ba732a308ee0e16c9
Combatd <combatdstar@gmail.com>
2023-10-08 17:21:41 -0700

    Define Flight::toString

--- prototypes/flightPrototype.js
+++ prototypes/flightPrototype.js
@@ -4,13 +4,28 @@ function Flight(airlines, flightNumber) {
 }
 
 // All flight objects will share the memory space to access this method
-Flight.prototype.display = function() {
-  console.log(this.airlines);
-  console.log(this.flightNumber);
+// Flight.prototype.display = function() {
+//   console.log(this.airlines);
+//   console.log(this.flightNumber);
+// }
+
+Flight.prototype = {
+  display:  function() {
+      console.log(this.airlines);
+      console.log(this.flightNumber);
+    },
+
+  toString: function() {
+    return "[Flight " + this.airlines + ", " + this.flightNumber + "]";
+  }
 }
+
 
 let flight1 = new Flight('American Airlines', 'AA123');
 let flight2 = new Flight('South West', 'SW456'); 
 
 flight1.display();
 flight2.display();
+
+console.log(flight1.toString());
+console.log(flight2.toString());
