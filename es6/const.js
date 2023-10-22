43955f0159656b261f28f3722fc0462a6e9bc068
Combatd <combatdstar@gmail.com>
2023-10-22 14:23:18 -0700

    Declare constant object

--- es6/const.js
+++ es6/const.js
@@ -1,3 +1,14 @@
+"use strict"; // if you want errors to be enforced, no silent failures
+
 const pi = 3.14;
 // pi = 3.79; // throws an error
 console.log(pi);
+
+const product = {
+  
+};
+// const product = Object.freese({}); // freeze the object
+// you can change attributes inside the object / hashmap
+product['name'] = 'Iphone';
+
+console.log(product);
