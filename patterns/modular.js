72b6d2be52a306ff7fd58c38589d556a0c5a0a76
Combatd <combatdstar@gmail.com>
2023-10-22 12:54:46 -0700

    Implement revealing module pattern

--- patterns/modular.js
+++ patterns/modular.js
@@ -21,3 +21,30 @@ account.balance = 2000; // does not work
 console.log(account.getBalance()); // => 1000
 account.addInterest(300);
 console.log(account.getBalance()); // => 1300
+
+// revealing module pattern
+var account2 = (function() {
+  // hidden encapsulated data, private scope
+  var balance = 1000;
+
+  function getBalance() {
+    return balance;
+  }
+
+  function addInterest(interest) {
+    balance += interest;
+  }
+
+  return {
+    name: 'Matt',
+    getBalance: getBalance,
+    addInterest, addInterest
+  }
+}());
+
+console.log(account2.name);
+console.log(account2.getBalance()); // => 1000
+account2.balance = 2000; // does not work
+console.log(account2.getBalance()); // => 1000
+account2.addInterest(300);
+console.log(account2.getBalance()); // => 1300
