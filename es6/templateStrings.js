ac2a334129b60675c8fb3a3ebf489e27f0ec1988
Combatd <combatdstar@gmail.com>
2023-10-24 00:30:35 -0700

    PUsh string interpolation of variables

--- es6/templateStrings.js
+++ es6/templateStrings.js
@@ -2,9 +2,11 @@ let quote = 'You are the\n'+
 'creator of\n'+
 'your destiny';
 
+let name = 'John'
+
 let quote2 = `You are the
 creator of 
-your destiny`;
+your destiny ${name}`;
 
 console.log(quote);
 console.log(quote2);
