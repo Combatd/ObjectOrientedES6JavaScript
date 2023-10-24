17e051291e1fc0b09da9a28d3ba44e82ecf79f24
Combatd <combatdstar@gmail.com>
2023-10-24 01:20:15 -0700

    destructure arrays

---
+++ es6/arrayDestructuring.js
@@ -0,0 +1,9 @@
+const courses = ['Angular', 'React', 'Node'];
+
+// const course1 = courses[0];
+
+const [course1, course2, course3] = courses;;
+
+console.log(course1);
+console.log(course2);
+console.log(course3);
